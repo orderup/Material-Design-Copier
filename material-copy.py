0d125537b30ae7149a1e0de8eb66be498110ee11
Cody Henthorne <cody@orderup.com>
2015-07-14 01:22:17 -0400

    Update script to handle icons in submodule and allow to specify size

--- material-copy.py
+++ material-copy.py
@@ -7,13 +7,14 @@ parser = argparse.ArgumentParser(description='Copy Image Assets from Material De
 parser.add_argument('group', type=str, help='The group eg. social, av, navigation')
 parser.add_argument('name', type=str, help='The name of the icon eg. 3d_rotation, attach_file, poll')
 parser.add_argument('color', type=str, help='color of the icon',choices=['black','white'])
+parser.add_argument('size', type=str, help='Size of icon',choices=['18','24','32','48'])
 parser.add_argument('dest', type=str, help='Desination res folder')
 args = parser.parse_args()
 dirs = map(lambda x: 'material-design-icons/'+args.group+'/drawable-'+x+'/', ['hdpi', 'mdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi'])
 
 for i in dirs:
-    src = i+'_'.join(['ic', args.name.lower(), args.color.lower(), '48dp.png'])
-    out_dir = args.dest+'/'+src.split('/')[1]
+    src = i+'_'.join(['ic', args.name.lower(), args.color.lower(), args.size + 'dp.png'])
+    out_dir = args.dest+'/'+src.split('/')[2]
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
         print ("Creating", out_dir)
